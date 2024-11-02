ef2f328ff2ff3b4e701f01cc1dcceb7106a5c874
Katherine Xie <kxie236@gmail.com>
2024-11-01 23:18:00 -0500

    limiting bins

--- newTwoParticleCorrJetFrame.C
+++ newTwoParticleCorrJetFrame.C
@@ -217,7 +217,7 @@ TH2F createEtaPhiDist_JetFrame(std::vector<Int_t> multiplicityVector,
         // Check to see if the event is in the multiplicity bin
         Int_t currEventIndex = reader.GetCurrentEntry();
         if (multiplicityVector[currEventIndex] == 0) {continue;}
-        if (multiplicityVector[eventIndex] < 186 || multiplicityVector[eventIndex] > 227) {continue;}
+        if (multiplicityVector[currEventIndex] < 186 || multiplicityVector[currEventIndex] > 227) {continue;}
 
 
         // Jet Loop
